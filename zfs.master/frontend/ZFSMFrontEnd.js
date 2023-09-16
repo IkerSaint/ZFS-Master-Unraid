e9781a5ced2d57f07b7bc8db96808731992509fd
IkerSaint <iker.saint@gmail.com>
2023-09-16 16:04:13 -0500

    Complete row generations for datasets

--- zfs.master/frontend/ZFSMFrontEnd.js
+++ zfs.master/frontend/ZFSMFrontEnd.js
@@ -1,4 +1,91 @@
-import * as utils from "./ZFSMUtils.js";
+// Utils
+
+function crc16(str) {
+	const crcTable = [];
+	const polynomial = 0xA001;
+  
+	for (let i = 0; i < 256; i++) {
+		let crc = i;
+		for (let j = 0; j < 8; j++) {
+			crc = (crc & 1) ? ((crc >>> 1) ^ polynomial) : (crc >>> 1);
+		}
+		crcTable[i] = crc;
+	}
+	
+	let crc = 0xFFFF;
+
+	for (let i = 0; i < str.length; i++) {
+		const charCode = str.charCodeAt(i);
+		crc = ((crc >>> 8) ^ crcTable[(crc ^ charCode) & 0xFF]) & 0xFFFF;
+	}
+	
+	return crc.toString(16).toUpperCase().padStart(4, '0');
+}
+
+function nl2br(str, is_xhtml) {
+    if (typeof str === 'undefined' || str === null) {
+        return '';
+    }
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+}
+
+function fromBytesToString(bytes) {
+	const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  
+	bytes = Math.max(bytes, 0);
+	const pow = Math.floor((bytes ? Math.log(bytes) : 0) / Math.log(1024));
+	const limitedPow = Math.min(pow, units.length - 1);
+  
+	bytes /= Math.pow(1024, limitedPow);
+  
+	return bytes.toFixed(2) + ' ' + units[limitedPow];
+  }
+
+function fromStringToBytes(spacestr) {
+	let returnNumber = parseFloat(spacestr);
+
+	switch (spacestr.slice(-1)) {
+	  case 'T':
+		returnNumber *= 1024;
+	  case 'G':
+		returnNumber *= 1024;
+	  case 'M':
+		returnNumber *= 1024;
+	  case 'K':
+		returnNumber *= 1024;
+	  default:
+		break;
+	}
+
+	return returnNumber;
+}
+
+function calculateFreePercent(used, free) {
+	const usedTmp = typeof used === "string" ? fromStringToBytes(used) : used;
+	const freeTmp = typeof free === "string" ? fromStringToBytes(free) : free;
+
+	const result = freeTmp / (freeTmp + usedTmp);
+	return result * 100;
+}
+
+function implodeWithKeys(glue, array, symbol = ': ') {
+    return Object.keys(array)
+	  .map((key) => key + symbol + array[key])
+	  .join(glue);
+}
+
+function daysToNow(timestamp) {
+    const currentDate = new Date();
+    const diffDate = new Date(timestamp * 1000);
+
+    const timeDifference = currentDate - diffDate; 
+
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    return daysDifference;
+}
+
 
 function getLastSnap(zsnapshots) {
 	var lastsnap = zsnapshots[0];
@@ -66,7 +153,7 @@ function generatePoolTableRows(zpool, devices, show_status) {
 	const status_msg = getPoolStatusMsg(zpool['Health']);
 
 	// Name and devices
-	var tr = '<td id="zpool-attribute-pool"><a class="info hand"><i id="zpool-'+zpool['Pool']+'" class="fa fa-circle orb '+status_color+'-orb"></i><span>'+utils.nl2br(devices)+'</span></a> '+zpool['Pool']+'</td>';
+	var tr = '<td id="zpool-attribute-pool"><a class="info hand"><i id="zpool-'+zpool['Pool']+'" class="fa fa-circle orb '+status_color+'-orb"></i><span>'+nl2br(devices)+'</span></a> '+zpool['Pool']+'</td>';
 
 	// Health
 	tr += '<td id="zpool-attribute-health"><a class="info hand"><i class="fa fa-heartbeat" style="color:'+status_color+'"></i><span>'+status_msg+'</span></a> '+zpool['Health']+'</td>';
@@ -85,7 +172,7 @@ function generatePoolTableRows(zpool, devices, show_status) {
 	tr += '<td id="zpool-attribute-refer">'+zpool['Refer']+'</td>'; 
 
 	// Used
-	var percent = 100-Math.round(utils.calculateFreePercent(zpool['Used'], zpool['Free']));
+	var percent = 100-Math.round(calculateFreePercent(zpool['Used'], zpool['Free']));
 	tr += '<td id="zpool-attribute-used"><div class="usage-disk"><span style="position:absolute; width:'+percent+'%" class=""><span>'+zpool['Used']+'B</span></div></td>';
 
 	// Free
@@ -107,7 +194,7 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 		'Creation Date' : creationDate.toISOString(),
 		'Compression' : zdataset['compression'],
 		'Compress Ratio' : zdataset['compressratio']/100,
-		'Record Size' : utils.fromBytesToString(zdataset['recordsize']),
+		'Record Size' : fromBytesToString(zdataset['recordsize']),
 		'Access Time' : zdataset['atime'],
 		'XAttr' : zdataset['xattr'],
 		'Primary Cache' : zdataset['primarycache'],
@@ -118,7 +205,7 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 		'Case Sensitive' : zdataset['casesensitivity'],
 		'Sync' : zdataset['sync'],
 		'Origin' : zdataset['origin'] ?? '',
-		'Space used by Snaps' : utils.fromBytesToString(zdataset['usedbysnapshots'])
+		'Space used by Snaps' : fromBytesToString(zdataset['usedbysnapshots'])
 	};
 
 	const icon_color = 'grey';
@@ -128,7 +215,7 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 
 		snapdate = new Date(snap['creation']);
 				
-		if (utils.daysToNow(snap['creation']) > snap_max_days_alert) {
+		if (daysToNow(snap['creation']) > snap_max_days_alert) {
 			icon_color = 'orange';
 		} else {
 			icon_color = '#486dba';
@@ -145,7 +232,7 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 	}
 
 	tr += '<a class="info hand"><i class="fa fa-hdd-o icon" style="color:'+icon_color+'" onclick="toggleDataset(\''+zdataset['name']+'\');"></i>';
-	tr += '<span>'+utils.implodeWithKeys('<br>', properties)+'</span></a>';
+	tr += '<span>'+implodeWithKeys('<br>', properties)+'</span></a>';
 
 
 	if (zdataset['child'] > 0) {
@@ -170,7 +257,7 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 	// Actions
 
 	tr += '<td>';
-	var id = utils.crc16(zdataset['name']);
+	var id = crc16(zdataset['name']);
 	var snap_count = 0;
 
 	if (zdataset['snapshots'] !== undefined ) {
@@ -197,18 +284,18 @@ function generateDatasetRow(zpool, zdataset, show_status, destructive_mode, snap
 
 	// Referr
 	tr += '<td>';
-	tr += utils.fromBytesToString(zdataset['referenced']);
+	tr += fromBytesToString(zdataset['referenced']);
 	tr += '</td>';
 
 	// Used
 	tr += '<td>';
-	var percent = 100-Math.round(utils.calculateFreePercent(zdataset['used'], zdataset['available']));
-	tr += '<div class="usage-disk"><span style="width:'+percent+'%" class=""><span>'+utils.fromBytesToString(zdataset['used'])+'</span></div>';
+	var percent = 100-Math.round(calculateFreePercent(zdataset['used'], zdataset['available']));
+	tr += '<div class="usage-disk"><span style="width:'+percent+'%" class=""><span>'+fromBytesToString(zdataset['used'])+'</span></div>';
 	tr += '</td>';
 
 	// Free
 	tr += '<td>';
-	tr += '<div class="usage-disk"><span style="width:'+(100-percent)+'%" class=""><span>'+utils.fromBytesToString(zdataset['available'])+'</span></div>';
+	tr += '<div class="usage-disk"><span style="width:'+(100-percent)+'%" class=""><span>'+fromBytesToString(zdataset['available'])+'</span></div>';
 	tr += '</td>';
 
 	// Snapshots
